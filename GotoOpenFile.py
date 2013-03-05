1246c7f6ad7fe791c859603b83eced22e3156d91
Philip Oliver-Paull <oliverpa@mailbox.sc.edu>
2013-03-05 09:33:28 -0500

    introduce "sort_views" setting just in case someone wants the views unsorted

--- GotoOpenFile.py
+++ GotoOpenFile.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin, os
 
+SETTINGS_NAME = 'GotoOpenFile'
+SETTINGS_FILE_NAME = '%s.sublime-settings' % SETTINGS_NAME
+
 class GotoOpenFileCommand(sublime_plugin.TextCommand):
 
   def run(self, edit, active_group=False):
@@ -16,7 +19,9 @@ class ViewSelector(object):
       self.views = window.views_in_group(window.active_group())
     else:
       self.views = window.views()
-    self.items = sorted([[self.__get_display_name(view), self.__get_path(view)] for view in self.views])
+    self.items = [[self.__get_display_name(view), self.__get_path(view)] for view in self.views]
+    if self.__get_setting('sort_views'):
+      self.items = sorted(self.items)
 
   def select(self, index):
     if index != -1:
@@ -56,4 +61,15 @@ class ViewSelector(object):
         return relpath
 
     return view.file_name()
+
+  def __get_setting(self, key):
+    view_settings = self.window.active_view().settings()
+    if view_settings.has(SETTINGS_NAME):
+      project_settings = view_settings.get(SETTINGS_NAME)
+      for proj_setting_key in project_settings:
+        if proj_setting_key == key:
+          return project_settings[proj_setting_key]
+
+    plugin_settings = sublime.load_settings(SETTINGS_FILE_NAME)
+    return plugin_settings.get(key, None)
 
